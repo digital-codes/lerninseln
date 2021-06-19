9bd15aa52c2b36b1941fa27785f18edd2530f01f
Andreas Kugel <andreas.kugel@web.de>
2021-06-19 22:57:30 +0200

    update

---
+++ src/components/Cal.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
+</template>
+
+<script>
+
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
+
+export default {
+    name: "Cal",
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarPlugins: [ dayGridPlugin, interactionPlugin ]
+    }
+  }
+}
+</script>
