b61c6d582c971a6b70a921649410973377833479
Andreas Kugel <andreas.kugel@web.de>
2021-06-20 10:35:17 +0200

    update

--- src/components/Cal.vue
+++ src/components/Cal.vue
@@ -1,23 +1,39 @@
-
 <template>
-  <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
+    <div>
+		<calendar-view
+			:show-date="showDate"
+			class="theme-default">
+			<template #header="{ headerProps }">
+				<calendar-view-header
+					:header-props="headerProps"
+					@input="setShowDate" />
+			</template>
+		</calendar-view>
+    </div>
 </template>
-
 <script>
+// https://github.com/richardtallent/vue-simple-calendar
 
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction';
+	import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+	
+	import "vue-simple-calendar/dist/style.css"
+	// The next two lines are optional themes
+	import "vue-simple-calendar/static/css/default.css"
 
-export default {
-    name: "Cal",
-  components: {
-    FullCalendar // make the <FullCalendar> tag available
-  },
-  data() {
-    return {
-      calendarPlugins: [ dayGridPlugin, interactionPlugin ]
-    }
-  }
-}
+	export default {
+		name: 'Cal',
+		data: function() {
+			return { showDate: new Date() }
+		},
+		components: {
+			CalendarView,
+			CalendarViewHeader,
+		},
+		methods: {
+			setShowDate(d) {
+				this.showDate = d;
+			},
+		}
+	}
 </script>
+
