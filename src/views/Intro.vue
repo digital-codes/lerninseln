08d7dcd511ecbb435b46cd3e8f49a0094760f01c
Andreas Kugel <andreas.kugel@web.de>
2021-06-20 17:16:41 +0200

    update

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -12,17 +12,43 @@
         </ion-toolbar>
       </ion-header>
     
-      <ExploreContainer name="Intro page" />
+      <ion-grid>
+    <ion-row>
+      <ion-col>
+    <ion-card>
+    <ion-card-header>
+      <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+      <ion-card-title>Card Title</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content>
+      Keep close to Nature's heart... and break clear away, once in awhile,
+      and climb a mountain or spend a week in the woods. Wash your spirit clean.
+    </ion-card-content>
+  </ion-card>
+
+      </ion-col>
+    </ion-row>
+    <ion-row>
+      <ion-col>
+      <!-- login -->
+      <ion-input type="email" placeholder="Email"></ion-input>
+      <ion-input type="password" placeholder="Password"></ion-input>
+      </ion-col>
+    </ion-row>
+      </ion-grid>
+
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonRow  } from '@ionic/vue';
+//import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  {
   name: 'Intro',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage,IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonRow }
 }
 </script>
