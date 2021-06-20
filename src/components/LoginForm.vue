ddaab299e84517bc72f0793e5988c15d1e9c8a60
Andreas Kugel <andreas.kugel@web.de>
2021-06-20 20:47:04 +0200

    update

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,83 @@
+<template>
+    <ion-card>
+    <ion-card-header>
+      <ion-card-title>Card Title</ion-card-title>
+      <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+    </ion-card-header>
+
+    <ion-card-content>
+      Keep close to Nature's heart... and break clear away, once in awhile,
+      and climb a mountain or spend a week in the woods. Wash your spirit clean.
+    </ion-card-content>
+  </ion-card>
+
+    <ion-card>
+    <ion-card-header>
+      <ion-card-title>Login Form</ion-card-title>
+      <ion-card-subtitle>... fake, so ...</ion-card-subtitle>
+    </ion-card-header>
+    <ion-card-content>
+      <!-- login -->
+      <ion-input 
+        required=true 
+        type="email" 
+        placeholder="Email" 
+        v-model="email" 
+        ></ion-input>
+      <ion-input 
+        type="password" 
+        placeholder="Password" 
+        v-model="pwd" 
+        ></ion-input>
+        <ion-button @click="submit">Submit</ion-button>
+    </ion-card-content>
+  </ion-card>
+
+</template>
+
+<script lang="ts">
+import { IonInput, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+//import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  {
+  name: 'LoginForm',
+  components: { IonInput, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle },
+  data: function() {
+    return {
+      email: "",
+      pwd: "",
+    } 
+  },
+  methods: {
+      submit() {
+        const credentials = {"email":this.email,"pwd":this.pwd}
+        console.log("WARNING!!!", credentials)
+        this.email = ""
+        this.pwd = ""
+      },
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -12,30 +12,7 @@
         </ion-toolbar>
       </ion-header>
     
-    <ion-card>
-    <ion-card-header>
-      <ion-card-title>Card Title</ion-card-title>
-      <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-    </ion-card-header>
-
-    <ion-card-content>
-      Keep close to Nature's heart... and break clear away, once in awhile,
-      and climb a mountain or spend a week in the woods. Wash your spirit clean.
-    </ion-card-content>
-  </ion-card>
-
-    <ion-card>
-    <ion-card-header>
-      <ion-card-title>Login Form</ion-card-title>
-      <ion-card-subtitle>... fake, so ...</ion-card-subtitle>
-    </ion-card-header>
-    <ion-card-content>
-      <!-- login -->
-      <ion-input type="email" placeholder="Email" :email="email" ></ion-input>
-      <ion-input type="password" placeholder="Password" :pwd="pwd" ></ion-input>
-    </ion-card-content>
-  </ion-card>
-
+    <LoginForm></LoginForm>
 
     </ion-content>
   </ion-page>
@@ -44,10 +21,11 @@
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 //import ExploreContainer from '@/components/ExploreContainer.vue';
+import LoginForm from '@/components/LoginForm.vue';
 
 export default  {
   name: 'Intro',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle },
+  components: { LoginForm, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
   data: function() {
     return {
       email: "",
