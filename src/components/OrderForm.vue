80a57b42f203f8d0bbd93253bd4ede2e3fe0aba8
Andreas Kugel <andreas.kugel@web.de>
2021-06-30 08:07:07 +0200

    update

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -9,14 +9,14 @@
       You can place any elements you want inside a form. The inputs themselves
       can even be deeply nested.
     </p>
-    <input v-model="formValues.name"
+    <input v-model="formValues.name" class="formulate-input formulate-input-element"
       name="name"
       type="text"
       label="Your name"
       placeholder="Your name"
       validation="required"
     />
-    <input  v-model="formValues.email"
+    <input  v-model="formValues.email" class="formulate-input formulate-input-element"
       name="email"
       type="email"
       label="Email address"
@@ -24,14 +24,14 @@
       validation="required|email"
     />
     <div class="double-wide">
-      <input  v-model="formValues.pwd"
+      <input  v-model="formValues.pwd" class="formulate-input formulate-input-element"
         name="password"
         type="password"
         label="Password"
         placeholder="Your password"
         validation="required"
       />
-      <input  v-model="formValues.pwd2"
+      <input  v-model="formValues.pwd2" class="formulate-input formulate-input-element"
         name="password_confirm"
         type="password"
         label="Confirm your password"
@@ -40,7 +40,7 @@
         validation-name="Confirmation"
       />
     </div>
-    <input 
+    <input  class="formulate-input formulate-input-element"
       type="submit"
       label="Register"
       value="Register"
@@ -51,15 +51,69 @@
     />
   </form>
 
+  <ion-card>
+  <ion-card-header>
+    <ion-card-title>Login Form</ion-card-title>
+    <ion-card-subtitle>Anmeldung nur für Buchung nötig, Stöbern geht auch ohne ...</ion-card-subtitle>
+  </ion-card-header>
+  <ion-card-content>
+  <form id="signup-form2"
+    class="login-form"
+  >
+    <h2 class="form-title">Register</h2>
+    <p>
+      You can place any elements you want inside a form. The inputs themselves
+      can even be deeply nested.
+    </p>
+    <ion-item>
+    <ion-label position="stacked">Stacked Label</ion-label>
+    <ion-input v-model="formValues.name" class="formulate-input formulate-input-element"
+      name="name"
+      type="text"
+      label="Your name"
+      placeholder="Your name"
+      validation="required"
+    />
+  </ion-item>
+
+    <ion-item>
+    <ion-label position="fixed">OK</ion-label>
+    <ion-checkbox
+        slot="start"
+        @update:modelValue="formValues.isChecked = $event"
+        :modelValue="formValues.isChecked">
+      </ion-checkbox>    
+    <ion-input  class="formulate-input formulate-input-element"
+      type="submit"
+      label="Register"
+      value="Register"
+    />
+  </ion-item>
+    <pre
+      class="code"
+      v-text="formValues"
+    />
+    <div v-if="formValues.isChecked">
+      isChecked
+    </div>
+
+  </form>
+  </ion-card-content>
+  </ion-card>
+
 </template>
 
 <script>
 // inspired by https://vueformulate.com/guide/forms/
 
+import { IonItem, IonLabel, IonInput, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 import { defineComponent } from 'vue'; 
 
 export default defineComponent ({
   name: "OrderForm",
+  components: {
+    IonItem, IonLabel, IonInput, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle 
+  },
   data () {
     return {
       formValues: {}
@@ -92,9 +146,12 @@ export default defineComponent ({
 .form-title {
   margin-top: 0;
 }
-.login-form::v-deep .formulate-input .formulate-input-element {
+
+/*.login-form::v-deep .formulate-input .formulate-input-element {*/
+:deep(<.login-form>) .formulate-input .formulate-input-element {
   max-width: none;
 }
+
 @media (min-width: 420px) {
   .double-wide {
     display: flex;
