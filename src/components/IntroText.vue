11ac7bba30125b29c49e54a5fb6ae97d4e780318
Andreas Kugel <andreas.kugel@web.de>
2021-12-04 17:20:04 +0100

    Update

--- src/components/IntroText.vue
+++ src/components/IntroText.vue
@@ -1,19 +1,39 @@
 <template>
     <ion-card>
     <ion-card-header>
-      <ion-card-title class="hdr">Eine Initiative der Stadt Karlsruhe</ion-card-title>
+      <ion-card-title class="hdr">Lerninseln Karlsruhe</ion-card-title>
     </ion-card-header>
 
     <ion-card-content>
-        Die Idee der „Lerninseln“ ist es, schulische sowie außerschulische Lernorte aufzuzeigen, die
-        die unterschiedlichsten Angebote machen und das Lernen in der „realen Welt“ ermöglichen.
-        Diese Orte werden in einer interaktiven Karte sichtbar und zugänglich gemacht. 
+      Lerninseln sind schulische und außerschulische Orte im Stadtgebiet Karlsruhe, 
+      an denen junge Menschen in Eigeninitiative oder mit Betreuung lernen können. 
+      Die Lerninseln ermöglichen allen Kindern und Jugendlichen jeder 
+      Schulform sowie Studierenden, auch unabhängig von Unterrichts- und Lehrbetrieb, 
+      unter besten Voraussetzungen zu lernen, allein oder in einer Gemeinschaft 
+      zu arbeiten und eigenen Interessen vertieft nachzugehen. 
+      <br>
+      Eine Lerninsel kann ein Arbeitsplatz mit Computer und Internetanschluss sein. 
+      Sie kann ein Raum sein, in dem Gruppen arbeiten können. 
+      Zudem kann eine Lerninsel pädagogische Unterstützung anbieten.
+      <br>
 
-        <ion-img src="/assets/img/front/1.jpg"></ion-img>
+      Die Stadt Karlsruhe sammelt geeignete Angebote und stellt mit 
+      einer interaktiven Karte einen Rahmen bereit, 
+      in dem diese übersichtlich dargestellt und unkompliziert gebucht werden können.
+      <br>
+
+      Die einzelnen Angebote finden in den Räumen und unter der Verantwortung 
+      der jeweiligen Anbieter statt.
+
+        <ion-img class="intro-image" src="/assets/img/front/1.jpg"></ion-img>
 
 
         <div v-if="isMobile()">
           <div>
+          <ion-button  @click="more()">
+            <ion-icon icon="/assets/icon/li-icon.svg" />
+          <ion-label>Mehr Infos</ion-label>
+          </ion-button>
           <ion-button  href="mailto:lerninseln@ok-lab-karlsruhe.de">
             <ion-icon :icon="mailOutline" />
           <ion-label>Schreib uns</ion-label>
@@ -26,6 +46,10 @@
         </div>
         <div v-else>
           <div>
+          <ion-button  :href="moreUrl" target="_blank">
+            <ion-icon icon="/assets/icon/li-icon.svg" alt="Impressum"/>
+          <ion-label>Mehr Infos</ion-label>
+          </ion-button>
           <ion-button  href="mailto:lerninseln@ok-lab-karlsruhe.de">
             <ion-icon :icon="mailOutline" />
           <ion-label>Schreib uns</ion-label>
@@ -45,8 +69,14 @@ import { defineComponent } from 'vue';
 
 import { useStore } from '../services/quickStore';
 
+import { Plugins } from '@capacitor/core';
+const { Browser } = Plugins;
+
 // https://capacitorjs.com/docs/apis/share
 import { Share } from '@capacitor/share';
+
+// custom icons: https://www.joshmorony.com/custom-svg-icons-in-ionic-with-ionicons/
+
 
 import { 
   shareSocialOutline,
@@ -72,11 +102,16 @@ export default defineComponent ({
       console.log("Platform: ",this.store.state.device.platform)
       return this.store.state.device.platform != "web"
     },
+    more() {
+      Browser.open({ 'url': this.moreUrl })
+    },
   },
   setup() {
     const store = useStore()
+    const moreUrl = "https://lerninseln.com"
     return {
       store,
+      moreUrl,
       mailOutline,shareSocialOutline,
     }
   },
@@ -122,5 +157,9 @@ export default defineComponent ({
   color: unset;  
 }
 
+.intro-image {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+}
 
 </style>
